d854ce2b709005bba1fd7b22bb4118ce09770f78
CoderPythonist890 <98164167+CoderPythonist890@users.noreply.github.com>
2022-02-16 23:04:54 +0100

    Create vid 31 KungFu logo.py

---
+++ vid 31 KungFu logo.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+goto(0, -205)
+width(10)
+circle(200, 360)
+width(2)
+home()
+begin_fill()
+seth(180)
+circle(100, 180)
+home()
+circle(100, 180)
+seth(0)
+for i in range(181):
+	fd(3.5)
+	rt(1)
+end_fill()
+pu()
+goto(-10, 100)
+dot(50)
+goto(10, -100)
+color("white")
+dot(50)
+
+done()
